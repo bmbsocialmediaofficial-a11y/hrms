0362fb69fc9f3078f8c1035ef9877734fef3ba24
bmbsocialmediaofficial-a11y <bmbsocialmediaofficial@gmail.com>
2025-09-22 13:55:11 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 // config.php
 
 // Database configuration
-$servername = "sql210.infinityfree.com";
-$username = "if0_39900894";
-$password = "g5J2pBIvpKrRSLT";
-$dbname = "if0_39900894_employee_db";
+$servername = "metro.proxy.rlwy.net";
+$username = "root";
+$password = "MhJRDhatBtkMwGCgOxizGHkVednZSUBj";
+$dbname = "railway";
 
 // Define the secure session encryption key (must be 32 bytes after base64 decode)
 define('SESSION_ENCRYPTION_KEY', base64_decode('v6K7f8Q9pL2sJ5hG1mN3cX4wZ8bV0tY7rE6uI9oP1aSdFgHjKlMnBvCxZqWeRtY'));
