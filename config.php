461158329ffb2715f1ef229ccd548365c3c2c0ef
bmbsocialmediaofficial-a11y <bmbsocialmediaofficial@gmail.com>
2025-09-22 23:04:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ $servername = "metro.proxy.rlwy.net"; // Railway public TCP host
 $username   = "root";                  // Railway username
 $password   = "MhJRDhatBtkMwGCgOxizGHkVednZSUBj"; // Railway password
 $dbname     = "railway";               // Railway database name
-$port       = 32231;                    // Railway port
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
