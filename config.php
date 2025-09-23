ee29c534410cb509a34ae790b54e1f47cfef4fa7
bmbsocialmediaofficial-a11y <bmbsocialmediaofficial@gmail.com>
2025-09-23 23:38:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,18 @@
-
-<!-- $servername = "metro.proxy.rlwy.net"; // Railway public TCP host
-$username   = "root";                  // Railway username
-$password   = "MhJRDhatBtkMwGCgOxizGHkVednZSUBj"; // Railway password
-$dbname     = "railway";               // Railway database name
-
+<?php
+// Database configuration
+$servername = "serverless-northeurope.sysp0000.db3.skysql.com";
+$username   = "dbpbf19790723";
+$password   = "YTyO6cwigcG9c0OBHk]h9KG";
+$dbname     = "sky";
+$port       = 4115;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
 ?>
- -->
