fbaf089539a6afb257c18a904ab8f74e33684fa7
bmbsocialmediaofficial-a11y <bmbsocialmediaofficial@gmail.com>
2025-09-23 23:46:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 //Include the configuration file
 require_once 'config.php';
 
-Create connection
+//Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-Check connection
+//Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
