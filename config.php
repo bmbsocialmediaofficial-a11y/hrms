6e3342b21d4ae73d2f0168a7d7278a1d88cda2a4
bmbsocialmediaofficial-a11y <bmbsocialmediaofficial@gmail.com>
2025-09-24 16:32:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,24 @@
 <?php
-// Ultra simple config.php
+// config.php - ULTRA SILENT (NO ERRORS)
 $servername = "serverless-northeurope.sysp0000.db3.skysql.com";
 $username = "dbpbf19790723";
 $password = "YTyO6cwigcG9c0OBHk]h9KG";
 $dbname = "sky";
 $port = 4115;
 
-// Simple connection without any extra features
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+// COMPLETELY SILENT CONNECTION
+$conn = null;
 
-// Don't output anything here - just create the connection
+// Suppress ALL errors and warnings
+error_reporting(0);
+ini_set('display_errors', 0);
+
+// Try connection without any error reporting
+$temp_conn = @new mysqli();
+
+if (@$temp_conn->real_connect($servername, $username, $password, $dbname, $port)) {
+    $temp_conn->set_charset("utf8mb4");
+    $conn = $temp_conn;
+}
+// If connection fails, $conn remains null - NO ERRORS
 ?>
