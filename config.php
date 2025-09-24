8a38bf8ff2e88e45674b8a6675ef908859b21254
bmbsocialmediaofficial-a11y <bmbsocialmediaofficial@gmail.com>
2025-09-24 12:02:15 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,13 @@
 <?php
-// config.php - Simple and safe version
+// Ultra simple config.php
 $servername = "serverless-northeurope.sysp0000.db3.skysql.com";
 $username = "dbpbf19790723";
 $password = "YTyO6cwigcG9c0OBHk]h9KG";
 $dbname = "sky";
 $port = 4115;
 
-// Simple connection - no advanced error reporting
+// Simple connection without any extra features
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Basic error check
-if ($conn->connect_error) {
-    // Don't show detailed errors to users
-    error_log("Database connection failed: " . $conn->connect_error);
-    // Set connection to null instead of dying
-    $conn = null;
-}
-
-// If connection is successful, set charset
-if ($conn && !$conn->connect_error) {
-    $conn->set_charset("utf8mb4");
-}
+// Don't output anything here - just create the connection
 ?>
